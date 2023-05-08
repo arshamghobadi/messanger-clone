07b742e06185fe61d884a0757da838874dd85f85
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-05-08 21:22:21 +0100

    craete and setup for meesanger clone

---
+++ app/(site)/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div
+      className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8
+   bg-gray-100
+  "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          className="mx-auto w-auto"
+          alt="logo"
+          src="/images/logo.png"
+          width={48}
+          height={48}
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
+}
